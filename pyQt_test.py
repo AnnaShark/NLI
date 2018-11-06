c7ec26a7be1044395dd612d82f9b1244f189f2d4
xavier gabarro <xaviergabarro@gmail.com>
2018-11-06 21:43:30 +0100

    first commit

---
+++ pyQt_test.py
@@ -0,0 +1,81 @@
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from speech2Text import SpeechToText
+import _thread
+
+
+
+class VisualSelector( QDialog):    
+    def __init__(self, parent=None):
+        super(VisualSelector, self).__init__(parent)
+        self.speechStarted = 0
+        self.drawRadioButtons()
+        self.drawSpeechButton()
+        mainLayout = QGridLayout()
+        mainLayout.addWidget(self.text2SpeechSelector)
+        mainLayout.addWidget(self.speechManager)
+         
+        self.setLayout(mainLayout)
+        self.setWindowTitle("SPEECH ")
+    def activate_speech(self):
+        if self.speechStarted == 1:
+            self.speechStarted = 0
+            print(self.speechStarted)
+            
+        else:
+            self.speechStarted = 1
+            print(self.speechStarted)
+             
+    def drawSpeechButton(self):
+        self.speechManager = QGroupBox();
+        activateButton = QPushButton('START')
+        activateButton.clicked.connect(self.activate_speech)
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("initiate or end the speech recognition"))
+        layout.addWidget(activateButton)
+        layout.addWidget(QPushButton('END'))
+        self.speechManager.setLayout(layout)
+        
+    def drawRadioButtons(self):
+        self.text2SpeechSelector = QGroupBox("Select speech Synthetizer")
+        pic = QLabel(self)
+        pic.setPixmap(QPixmap("icon.jpg"))
+        radioButton1 = QRadioButton("Speech Synth. 1")
+        radioButton2 = QRadioButton("Speech Synth. 2")
+        radioButton3 = QRadioButton("Speech Synth. 3")
+        radioButton1.setChecked(True)        
+
+        layout = QVBoxLayout()
+        layout.addWidget(pic)
+        layout.addWidget(radioButton1)
+        layout.addWidget(radioButton2)
+        layout.addWidget(radioButton3)
+        layout.addStretch(1)
+        self.text2SpeechSelector.setLayout(layout)
+
+def speech_thread (recognizer,view):
+    while recognizer.iterate == 1:
+        if view.speechStarted == 1:
+            print("is iterating: ", recognizer.iterate)
+            recognizer.getAudio()
+            print("++++++++++++++++++++++")
+def main():
+    QApplication.setStyle(QStyleFactory.create("Fusion"))
+    app = QApplication(sys.argv)
+    view = VisualSelector()
+    recognizer = SpeechToText()
+    recognizer.iterate = 1
+    print("started")
+    view.show()
+    _thread.start_new_thread(speech_thread,( recognizer, view,))
+    sys.exit(app.exec_())
+    
+    
+        
+if __name__ == '__main__':
+    import sys
+    main()
+   
+        
---
+++ speech2Text.py
@@ -0,0 +1,43 @@
+
+import speech_recognition as sr 
+import spacy
+from gtts import gTTS
+from pygame import mixer
+class SpeechToText():
+    
+    
+    def __init__(self, parent=None):
+        self.iterate = 0
+        self.canProcess = 0
+        self.r = sr.Recognizer()
+        self.r.pause_threshold = 0.8
+        self.nlp = spacy.load('en_core_web_sm')
+
+    
+    def speech2Txt(self):
+    
+        try:
+            print("Google Speech Recognition thinks you said " + self.r.recognize_google(self.audio))
+            canProcess = 1
+            doc = self.nlp(self.r.recognize_google(self.audio))
+            for token in doc:
+                print(token.text, token.pos_, token.dep_)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+            canProcess = 0
+        except sr.RequestError as e:
+            canProcess = 0
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+   
+    def getAudio(self):
+        with sr.Microphone() as source:
+            print("Say something!")
+            self.audio = self.r.listen(source)
+            print("listening...")
+        self.speech2Txt()
+
+
+    
+
+        
