821ee4929f45775850ce66da90832da205674921
xavier gabarro <xaviergabarro@gmail.com>
2019-01-04 11:21:10 +0100

    speech generation improvement

--- pyQt_test.py
+++ pyQt_test.py
@@ -4,74 +4,82 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from speech2Text import SpeechToText
 from text2speech import Text2Speach as t2s
+from statemachine import StateMachine, State
+import time
 import _thread
 import json
 import random
 
-class VisualSelector( QDialog):    
-    def __init__(self, parent=None):
-        super(VisualSelector, self).__init__(parent)
-        self.speechStarted = 0
-        self.drawSpeechButton()
+class DialogManager(StateMachine):
+    #states
+    welcome = State('Welcome',initial = True)
+    basicData = State('basicData')
+    location = State('Location')
+    solve = State('Solve')
+    goodBye = State('Goodbye')
+    #transitions
+    transBasicData = welcome.to(basicData)
+    transLocation = basicData.to(location)
+    transSolve = location.to(solve)
+    transGoodBye = solve.to(goodBye)
+    
+    
+
+    #callbacks
+    def on_enter_basicData(self):
+        print(dm.current_state)
+        speak(typeOfMessage='askHowAreYou')
+        recognizer.getAudio()
+
+    def on_enter_location(self):
+        print(dm.current_state)
+        speak(typeOfMessage='HasUndestood')
         
-        mainLayout = QGridLayout()
-        mainLayout.addWidget(self.speechManager)
-         
-        self.setLayout(mainLayout)
-        self.setWindowTitle("SPEECH ")
-    def activate_speech(self):        
-        self.speechStarted = 1
-        print(self.speechStarted)
-    def deActivate_speech(self):        
-        self.speechStarted = 0
-        print(self.speechStarted)
-             
-    def drawSpeechButton(self):
-        self.speechManager = QGroupBox();
-        activateButton = QPushButton('START')
-        activateButton.clicked.connect(self.activate_speech)
-        deActivateButton = QPushButton('END')
-        deActivateButton.clicked.connect(self.deActivate_speech)
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("initiate or end the speech recognition"))
-        layout.addWidget(activateButton)
-        layout.addWidget(deActivateButton)
-        self.speechManager.setLayout(layout)
+    def on_enter_solve(self):
+        print(dm.current_state)
+        speak(typeOfMessage='HasntUndestood')
+        
+    def on_enter_goodBye(self):
+        print(dm.current_state)
+        speak(typeOfMessage='GoodBye')
 
     
 
-def speech_thread (recognizer,view):
-    
-    while recognizer.iterate == 1:
-        if view.speechStarted == 1:
-            print("is iterating: ", recognizer.iterate)
-            recognizer.getAudio()
-            print("++++++++++++++++++++++")
-        else:
-            print("stopped")
-            
+        
 def dataInitialization():
         with open('server_questions.json') as f:
             return json.load(f)
-
-def getAudioTosay(data,typeOfMessage):
-    return data[typeOfMessage][random.randint(0,len(data[typeOfMessage])-1)]
-                               
-def main():
-    data = dataInitialization()
-    dataInitialization()
-    QApplication.setStyle(QStyleFactory.create("Fusion"))
-    app = QApplication(sys.argv)
-    view = VisualSelector()
-    recognizer = SpeechToText()
-    recognizer.iterate = 1
-    currentState = "noInitialized"
+        
+dm = DialogManager()
+data = dataInitialization()
+recognizer = SpeechToText()
     
-    view.show()    
-    t2s.speak(getAudioTosay(data,typeOfMessage='welcome'))
+
+def speech_thread (recognizer):
+    
+    while recognizer.iterate == 1:        
+        recognizer.getAudio()
+        print("++++++++++++++++++++++")
+            
+
+
+def getAudioTosay(typeOfMessage):
+    return data[typeOfMessage][random.randint(0,len(data[typeOfMessage])-1)]
+
+def speak(typeOfMessage):
+    t2s.speak(sentence=getAudioTosay(typeOfMessage=typeOfMessage),fileName=str(time.time())+typeOfMessage)
+def main():
+    
+    
+  
+    
+    recognizer.iterate = 0
+    currentState = "noInitialized"
+    speak(typeOfMessage='welcome')
     print("started")
-    _thread.start_new_thread(speech_thread,( recognizer, view,))
-    sys.exit(app.exec_())
+    dm.transBasicData()
+    ##_thread.start_new_thread(speech_thread,( recognizer,))
+    
     
     
         
--- text2speech.py
+++ text2speech.py
@@ -2,12 +2,12 @@ from gtts import gTTS
 from pygame import mixer
 
 class Text2Speach():
-    def speak(sentence):
+    def speak(sentence,fileName):
         tts = gTTS(text=sentence, lang='en')
-        tts.save("test.mp3")
+        tts.save("speechs/"+fileName +".mp3")
 
         mixer.init()
-        mixer.music.load("test.mp3")
+        mixer.music.load("speechs/"+fileName +".mp3")
         mixer.music.play()
         while mixer.music.get_busy() == True:
             continue
